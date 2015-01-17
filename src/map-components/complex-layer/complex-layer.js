d7f50aa2544347d87039d562112423e3b692999e
Netanel Gilad <netanelgilad@gmail.com>
2015-01-17 15:26:57 +0200

    added complex-layer

---
+++ src/map-components/complex-layer/complex-layer.js
@@ -0,0 +1,43 @@
+/**
+ * Created by netanel on 17/01/15.
+ */
+angular.module('angularCesium').directive('complexLayer', function($log) {
+  return {
+    restrict : 'E',
+    require : '^map',
+    compile : function(element, attrs) {
+      if (attrs.observableCollection) {
+        angular.forEach(element.children(), function (child) {
+
+          var layer = undefined;
+
+          if (child.tagName === 'BILLBOARD') {
+            layer = angular.element('<billboards-layer></billboards-layer>');
+          }
+          else if (child.tagName === 'LABEL') {
+            layer = angular.element('<labels-layer></labels-layer>');
+          }
+          else {
+
+          }
+
+          if (!layer) {
+            $log.warn('Found an unknown child of of complex-layer. Removing...');
+            angular.element(child).remove();
+          }
+          else {
+            angular.forEach(child.attributes, function (attr) {
+              layer.attr(attr.name, attr.value);
+            });
+            angular.forEach(element[0].attributes, function (attr) {
+              if (!angular.element(child).attr(attr.name)) {
+                layer.attr(attr.name, attr.value);
+              }
+            });
+            angular.element(child).replaceWith(layer);
+          }
+        });
+      }
+    }
+  }
+});
