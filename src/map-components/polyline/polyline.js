14b4b26861eebab3179929a70fce7ea89390d814
Gil Nisan <gilnis2@hotmail.com>
2015-01-18 16:51:49 +0200

    Added polylines layer and polyline to angular-cesium

--- src/map-components/polyline/polyline.js
+++ src/map-components/polyline/polyline.js
@@ -13,18 +13,21 @@ angular.module('angularCesium').directive('polyline', function() {
     link : function(scope, element, attrs, polylinesLayerCtrl) {
       var polylineDesc = {};
 
-      if (angular.isDefined(scope.positions) || angular.isFunction(scope.positions)){
+      if (!angular.isDefined(scope.positions) || !angular.isFunction(scope.positions)){
         throw "Polyline positions must be defined as a function";
       }
       var positions = scope.positions();
+      polylineDesc.positions = [];
       angular.forEach(positions, function(position) {
         polylineDesc.positions.push(Cesium.Cartesian3.fromDegrees(Number(position.latitude) || 0, Number(position.longitude) || 0, Number(position.altitude) || 0));
       });
 
-      polylineDesc.color = Cesium.Color.fromCssColorString('black');
+      var cesiumColor = Cesium.Color.fromCssColorString('black');
       if (angular.isDefined(scope.color) && angular.isFunction(scope.color)){
-          polylineDesc.color = scope.color();
+        cesiumColor = Cesium.Color.fromCssColorString(scope.color());
         }
+      polylineDesc.material = Cesium.Material.fromType('Color');
+      polylineDesc.material.uniforms.color = cesiumColor;
 
       polylineDesc.width = 1;
       if (angular.isDefined(scope.width) && angular.isFunction(scope.width)){
